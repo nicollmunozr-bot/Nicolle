97cf9ca584b295c6dedd1dd766670a9a1fa2a6d4
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-09-03 21:39:47 -0400

    JAVA-006 - Implementar Main y crear Estudiante

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public Main() {
+    }
 
+    public static void main(String[] args) {
         Estudiante estudiante = new Estudiante(
                 "Nicolle", "Ingeniería en Informática", 31);
 
         estudiante.mostrarInformacion();
+
+
     }
 }
